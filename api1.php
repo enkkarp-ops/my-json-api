f12919bdc58223dd708f0a15f62df8d84bab9d65
enkkarp-ops <enkkarp@gmail.com>
2026-02-06 21:06:42 +0300

    Add files via upload

---
+++ api1.php
@@ -0,0 +1,152 @@
+{
+	"launcher_version": 57,
+	"launcher_url": "https://crmp.pro/files/matrp/new/launcher_space.apk",
+	"launcher_path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/launcher_space.apk",
+	"launcher_name": "/launcher_space.apk",
+	"isTest": false,
+	"test_api": false,
+    "api": "https://raw.githubusercontent.com/enkkarp-ops/my-json-api/refs/heads/main/main.json",
+	"archives": [
+        {
+            "type": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb",
+            "zip_path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/archive_txd1.zip",
+            "size": 1231610996,
+            "urls": "https://crmp.pro/files/matrp/new/files/build8/archive_txd1.zip",
+            "paths": [
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/gta_int"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/gta3"
+                }
+            ]
+        },
+        {
+            "type": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb",
+            "zip_path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/archive_txd2.zip",
+            "size": 322070595,
+            "urls": "https://crmp.pro/files/matrp/new/files/build8/archive_txd2.zip",
+            "paths": [
+
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/gui"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/menu"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/mobile"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/player"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/radar"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/samp"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/txd"
+                }
+            ]
+        },
+        {
+            "type": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb",
+            "zip_path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/archive_img1.zip",
+            "size": 483414016,
+            "urls": "https://crmp.pro/files/matrp/new/files/build8/archive_img1.zip",
+            "paths": [
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/gta_int.img"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/player.img"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/samp.img"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/SAMPCOL.img"
+                }
+            ]
+        },
+        {
+            "type": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb",
+            "zip_path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/archive_img2.zip",
+            "size": 1222305792,
+            "urls": "https://crmp.pro/files/matrp/new/files/build8/archive_img2.zip",
+            "paths": [
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/texdb/gta3.img"
+                }
+            ]
+        },
+        {
+            "type": "/storage/emulated/0/Android/data/ru.edgar.matrp/files",
+            "zip_path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/archive_lite.zip",
+            "size": 119570433,
+            "urls": "https://crmp.pro/files/matrp/new/files/build8/archive_lite.zip",
+            "paths": [
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/data"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/images"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/models"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/SPACE"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/anim"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/audio"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/fonts"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/Text"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/Textures"
+                }
+            ]
+        },
+        {
+            "type": "/storage/emulated/0/Android/data/ru.edgar.matrp/files",
+            "zip_path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/archive_config.zip",
+            "size": 132146,
+            "urls": "https://crmp.pro/files/matrp/new/files/build8/archive_config.zip",
+            "paths": [
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/Adjustable.cfg"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/gta_sa.set"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/GTASAMP10.b"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/gtasatelem.set"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/stream.ini"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/version.txt"
+                },
+                {
+                    "path": "/storage/emulated/0/Android/data/ru.edgar.matrp/files/CINFO.BIN"
+                }
+            ]
+        }
+    ],
+    "deleted": [
+    ]
+}
